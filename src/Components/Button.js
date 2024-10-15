47e3ca8385d18052f38282d1d756a81c663fb673
akhons <akhilajith771@gmail.com>
2024-10-15 10:57:34 +0530

    changes to Button.js & HeroSection.css

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -12,9 +12,9 @@ const Button = ({ children, type, onClick, buttonStyle, buttonSize }) =>{
     const checkButtonSize = SIZES.includes( buttonSize ) ? buttonSize : SIZES[0];
 
     return(
-        <Link to='/contact' className='bin-mobile'>
+        <Link to='/contact' className='btn-mobile'>
             <button
-            className={`bin ${checkButtonStyle} ${checkButtonSize}`}
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
             onClick={onClick}
             type={type}
             >
